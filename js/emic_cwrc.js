af416f49767db047f3ebb8a51cf886831362f329
Alan Stanley <astanley@upei.ca>
2012-02-08 13:14:21 -0400

    Added internal callbacks

---
+++ js/emic_cwrc.js
@@ -0,0 +1,12 @@
+
+Drupal.behaviors.emic = function(context){
+  $('.cwrc_submit').hide();
+  $(".jumpmenu").change(function() {
+    var val = ($('.jumpmenu :selected').attr('value'));
+    if (val != '') {
+      //location.href=val;
+      window.open(val);
+    }
+  });
+};
+

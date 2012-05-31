c12dd550485beb760ab95b928260d643f7244e85
Alan Stanley <astanley@upei.ca>
2012-05-31 11:34:56 -0300

    fixed page count

--- resources/Islandora/js/startup.js
+++ resources/Islandora/js/startup.js
@@ -51,7 +51,7 @@ $('document').ready(function(){
   if(cwrc_params.position == 1){
     $('#page-prev').css('opacity', '.6').addClass('disabled');
   }
-  if(cwrc_params.position == cwrc_params.pages.length){
+  if(cwrc_params.position == cwrc_params.pages_count){
     $('#page-next').css('opacity', '.6').addClass('disabled');
   }
   // build and populate page choice dropdown
@@ -80,7 +80,7 @@ $('document').ready(function(){
     if(cwrc_params.position ==1 ){
       $('#page-prev').css('opacity', '.6').addClass('disabled');
     }
-    if(cwrc_params.position == cwrc_params.pages.length ){
+    if(cwrc_params.position == cwrc_params.page_count ){
       $('#page-next').css('opacity', '.2').addClass('disabled');
     }
     selector = "#page_choose option[value='" + cwrc_params.position + "']";
@@ -123,7 +123,7 @@ $('document').ready(function(){
       writer.fm.loadEMICDocument();
       init_canvas_div();
       $('#header h1').text( cwrc_params.title + " - Seq # " + (parseInt(cwrc_params.position)));
-      if(cwrc_params.position == cwrc_params.pages_count -1){
+      if(cwrc_params.position == cwrc_params.page_count){
         $('#page-next').css('opacity', '.2').addClass('disabled');
       }
     }

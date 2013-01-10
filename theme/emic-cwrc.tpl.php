98838635fc89d8aed6208c554d7e06319e1bf621
Alan Stanley <astanley@upei.ca>
2013-01-10 14:38:36 -0400

    Temporarily disabled resizing

--- theme/emic-cwrc.tpl.php
+++ theme/emic-cwrc.tpl.php
@@ -1,5 +1,5 @@
-<div class="emic-cwrc-wrapper">
-  <input id="full-window-button" type="button" value="<?php print t('Full Window'); ?>" />
+<div class="emic-cwrc-wrapper cwrc-fullwindow">
+ <!-- <input id="full-window-button" type="button" value="<?php print t('Full Window'); ?>" /> -->
   <input id="bookview_button" type="button" value="<?php print t('Return to Book View'); ?>" />
   <div class="cwrc-iframe-wrapper">
     <iframe id="shared_canvas_iframe" src="<?php print $src['src']; ?>" scrolling="0" frameborder="0" style="width: 100%; height: 800px;"><?php print t('Errors: unable to load SharedCanavas'); ?></iframe>
